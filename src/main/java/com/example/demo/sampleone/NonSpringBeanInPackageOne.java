f9fb5ce2d476579e4183f6442f4e06207d6777bc
Faraz Durrani <faraz.uic@gmail.com>
2019-07-17 09:58:56 -0500

    removed unneccessary files.

--- src/main/java/com/example/demo/sampleone/NonSpringBeanInPackageOne.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.sampleone;
-
-public class NonSpringBeanInPackageOne {
-	public String method() {
-		return "working";
-	}
-}
--- src/main/java/com/example/demo/sampletwo/NonSpringBeanInPackageTwo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.sampletwo;
-
-public class NonSpringBeanInPackageTwo {
-	public String method() {
-		return "working";
-	}
-}
